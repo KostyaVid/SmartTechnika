7a4ca838d9db9518f1ee4763df8e4be83aa5ec9a
KostyaVid <constat@live.com>
2022-04-04 14:13:31 +0300

    fix overflow

--- app/js/modals.js
+++ app/js/modals.js
@@ -1,23 +1,22 @@
 let passwordVisiableEnter = false;
 
+const offScroll = function(){
+   window.scrollTo(0, 0);
+}
+
 //Modal Enter
 document.querySelectorAll('.menu-profile ul li').forEach((element)=>{
       element.addEventListener('click', ()=>{
+      window.addEventListener(('scroll'), offScroll);
       document.getElementById('id-modal-enter').classList.add('modal_active');
       document.querySelector('.menu-profile').classList.remove('menu-profile_active');
-      document.querySelector('body').classList.add('body_overflow');
    });
 });
 
-document.querySelector('.menu-profile__enter').addEventListener('click', ()=>{
-   document.getElementById('id-modal-enter').classList.add('modal_active');
-   document.querySelector('.menu-profile').classList.remove('menu-profile_active');
-   document.querySelector('body').classList.add('body_overflow');
-});
 
 document.getElementById('id-modal-enter-exit').addEventListener('click', ()=>{
-document.getElementById('id-modal-enter').classList.remove('modal_active');
-document.querySelector('body').classList.remove('body_overflow');
+   document.getElementById('id-modal-enter').classList.remove('modal_active');
+   window.removeEventListener('scroll', offScroll);
 });
 
 //modal registr
@@ -25,13 +24,11 @@ document.querySelector('body').classList.remove('body_overflow');
 document.getElementById('modal__enter-registr_ID').addEventListener('click', ()=>{
    document.getElementById('id-modal-regidtr').classList.add('modal_active');
    document.querySelector('.menu-profile').classList.remove('menu-profile_active');
-   document.querySelector('body').classList.add('body_overflow');
    document.getElementById('id-modal-enter').classList.remove('modal_active');
 })
 
 document.getElementById('id-modal-registr-exit').addEventListener('click', ()=>{
-document.getElementById('id-modal-regidtr').classList.remove('modal_active');
-document.querySelector('body').classList.remove('body_overflow');
+   document.getElementById('id-modal-regidtr').classList.remove('modal_active');
 });
 
 document.getElementById('modal__i-already_asign-ID').addEventListener('click', ()=>{
